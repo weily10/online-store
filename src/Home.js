d958d7e961fa177f17cc5c90c020f8ec910a3ef7
weily10 <w3ily10@gmail.com>
2022-05-19 22:48:47 +0800

    ADD PRODUCT

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css'
+import Product from './Product';
 
 function Home() {
   return (
@@ -10,7 +11,24 @@ function Home() {
           src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1099"
           alt=""
         />
+        <div className="home__row">
+          <Product
+            id="12321341"
+            title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+            price={11.96}
+            rating={5}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+          />
+          <Product
+            id="49538094"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={239.0}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+          />
+        </div>
       </div>
+
 
   </div>)
 }
